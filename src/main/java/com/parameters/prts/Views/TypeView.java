6485accb836313aceda783ad5a1367f0ce57c215
Victory13004 <v.makarova1999@yandex.ru>
2021-04-07 03:30:16 +0300

    Views updated

--- src/main/java/com/parameters/prts/Views/TypeView.java
+++ src/main/java/com/parameters/prts/Views/TypeView.java
@@ -2,25 +2,167 @@ package com.parameters.prts.Views;
 
 import com.parameters.prts.Model.TypeEntity;
 import com.parameters.prts.Service.TypeService;
+import com.parameters.prts.Views.components.ConfirmationDialog;
 import com.parameters.prts.Views.main.MainView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.vaadin.artur.helpers.CrudServiceDataProvider;
+
+import java.util.Optional;
 
 @Route(value = "type", layout = MainView.class)
 @PageTitle("Тип")
 @CssImport("./views/type/type-view.css")
 public class TypeView extends VerticalLayout {
     private final TypeService typeService;
+    private final Grid<TypeEntity> grid = new Grid<>(TypeEntity.class, false);
 
-    private final Grid<TypeEntity> grid = new Grid<>(TypeEntity.class);
+    private TextField nameTp;
+
+    private Button cancel = new Button("Отмена");
+    private Button save = new Button("Сохранить");
+    private Button delete = new Button("Удалить");
+
+    private BeanValidationBinder<TypeEntity> binder;
+
+    private TypeEntity type;
 
     public TypeView(TypeService typeService) {
         this.typeService = typeService;
-        addClassName("type-view.css");
+        addClassName("type-view");
 
-        add(grid);
+        SplitLayout splitLayout = new SplitLayout();
+        splitLayout.setSizeFull();
+        createGridLayout(splitLayout);
+        createEditorLayout(splitLayout);
+        add(splitLayout);
+
+        grid.addColumn(TypeEntity::getNameTp).setHeader("Наименование").setAutoWidth(true);
+
+        grid.setDataProvider(new CrudServiceDataProvider<>(this.typeService));
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        grid.setHeightFull();
+
+        grid.asSingleSelect().addValueChangeListener(event -> {
+            if (event.getValue() != null) {
+                Optional<TypeEntity> typeFromBackend = this.typeService.get(event.getValue().getId());
+                if (typeFromBackend.isPresent()) {
+                    populateForm(typeFromBackend.get());
+                } else {
+                    refreshGrid();
+                }
+            } else {
+                clearForm();
+            }
+        });
+
+        binder = new BeanValidationBinder<>(TypeEntity.class);
+        binder.bindInstanceFields(this);
+
+        cancel.addClickListener(event -> {
+            clearForm();
+            refreshGrid();
+        });
+
+        save.addClickListener(event -> {
+            try {
+                if (this.type == null) {
+                    this.type = new TypeEntity();
+                }
+                binder.writeBean(this.type);
+
+                this.typeService.update(this.type);
+                clearForm();
+                refreshGrid();
+                Notification.show("Тип сохранён");
+            } catch (ValidationException validationException) {
+                Notification.show("Не удалось сохранить :(");
+            }
+        });
+
+        delete.addClickListener(event -> {
+            if (this.type != null && this.type.getId() != null) {
+                ConfirmationDialog confirmationDialog = new ConfirmationDialog("Удаление объекта",
+                        "Вы действительно хотите удалить объект " + this.type.getNameTp() + " ?", confirm -> {
+                    this.typeService.delete(this.type.getId());
+                    clearForm();
+                    refreshGrid();
+                    Notification.show("Тип удалён");
+                });
+                confirmationDialog.open();
+            }
+        });
+    }
+
+    private void createEditorLayout(SplitLayout splitLayout) {
+        Div editorLayoutDiv = new Div();
+        editorLayoutDiv.setId("editor-layout");
+
+        Div editorDiv = new Div();
+        editorDiv.setId("editor");
+        editorLayoutDiv.add(editorDiv);
+
+        FormLayout formLayout = new FormLayout();
+        nameTp = new TextField("Наименование");
+        Component[] fields = new Component[]{nameTp};
+
+        for (Component field : fields) {
+            ((HasStyle) field).addClassName("full-width");
+        }
+        formLayout.add(fields);
+        editorDiv.add(formLayout);
+        createButtonLayout(editorLayoutDiv);
+
+        splitLayout.addToSecondary(editorLayoutDiv);
+    }
+
+    private void createButtonLayout(Div editorLayoutDiv) {
+        VerticalLayout buttonLayout = new VerticalLayout();
+        buttonLayout.setId("button-layout");
+        buttonLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        buttonLayout.setSpacing(true);
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        cancel.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        buttonLayout.add(save, delete, cancel);
+        editorLayoutDiv.add(buttonLayout);
+    }
+
+    private void createGridLayout(SplitLayout splitLayout) {
+        Div wrapper = new Div();
+        wrapper.setId("grid-wrapper");
+        wrapper.setWidthFull();
+        splitLayout.addToPrimary(wrapper);
+        wrapper.add(grid);
+    }
+
+    private void refreshGrid() {
+        grid.select(null);
+        grid.getDataProvider().refreshAll();
+    }
+
+    private void clearForm() {
+        populateForm(null);
+    }
+
+    private void populateForm(TypeEntity typeEntity) {
+        this.type = typeEntity;
+        binder.readBean(this.type);
     }
 }
